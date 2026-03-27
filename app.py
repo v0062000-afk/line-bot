89e1b0d8a2449f9df52816f653288d401264aca7
v0062000-afk <v0062000@gmail.com>
2026-03-28 05:30:18 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -554,4 +554,5 @@ def handle_image_message(event):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render會給PORT
+    app.run(host="0.0.0.0", port=port)
